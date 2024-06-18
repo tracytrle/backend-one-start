eb45e3723f4878091e81caf069739d42d415ee4f
Truc Tran <tracytrle@gmail.com>
2024-06-18 15:42:40 -0500

    move controller to route

--- controllers/roberta_controller.py
+++
@@ -1,8 +0,0 @@
-from flask import request, jsonify
-from service.roberta_service import get_result
-
-def analyze_text():
-    input_text = request.args.get('input', "I wish I have a lot of money")
-    sentences = [input_text]
-    result = get_result(sentences)
-    return jsonify(result)
--- routes/roberta_router.py
+++ routes/roberta_router.py
@@ -1,8 +1,11 @@
-from flask import Blueprint
-from controllers.roberta_controller import analyze_text
+from flask import Blueprint, redirect, url_for, request, jsonify
+from service.roberta_service import get_result
 
 bp = Blueprint('roberta_router', __name__)
 
 @bp.route('/api/roberta', methods=['GET'])
-def get_roberta_result():
-    return "analyze_text()"
+def analyze_text():
+    input_text = request.args.get('input', "I wish I have a lot of money")
+    sentences = [input_text]
+    result = get_result(sentences)
+    return jsonify(result)
