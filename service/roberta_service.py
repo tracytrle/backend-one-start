fec2c49f42743622a02389aecdb1e5b9cde1e64a
Truc Tran <tracytrle@gmail.com>
2024-06-22 16:31:12 -0500

    add roberta

--- service/roberta_service.py
+++ service/roberta_service.py
@@ -1,6 +1,6 @@
-# from transformers import pipeline
-# classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
+from transformers import pipeline
+classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
 
 def get_result(sentences):
-  # return classifier(sentences)
-  return "roberta result"
+  return classifier(sentences)
+  
