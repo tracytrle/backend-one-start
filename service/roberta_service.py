c9106ba538438b8f42b5a4075a1f341f5df69279
Truc Tran <tracytrle@gmail.com>
2024-06-22 17:04:51 -0500

    only run on distilbert model

--- service/roberta_service.py
+++ service/roberta_service.py
@@ -1,7 +1,7 @@
-from transformers import pipeline
-classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
+# from transformers import pipeline
+# classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
 
 def get_result(sentences):
-  return classifier(sentences)
-  # return "This is a placeholder for the Roberta model."
+  # return classifier(sentences)
+  return "This is a placeholder for the Roberta model."
   
