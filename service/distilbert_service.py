90996b4ebd76842dcbbab8eb1b2b2dba04f610e3
Truc Tran <tracytrle@gmail.com>
2024-06-22 16:45:18 -0500

    try with only roberta model

--- service/distilbert_service.py
+++ service/distilbert_service.py
@@ -1,11 +1,12 @@
-from transformers import DistilBertTokenizer, DistilBertForSequenceClassification, pipeline
+# from transformers import DistilBertTokenizer, DistilBertForSequenceClassification, pipeline
 
 
-tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
-model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
-sentiment_classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
+# tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+# model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+# sentiment_classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
 
 
 def get_result(sentences):
-  return sentiment_classifier(sentences)
+  # return sentiment_classifier(sentences)
+  return "This is a placeholder for the DistilBERT model."
   
